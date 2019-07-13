da6f5e42bd6ae0f3ce97325a743b66957f2f19b5
Wataru Kume <>
2019-07-14 00:16:32 +0900

    add model spec: check a project done

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Project do
     project.tasks << task
     expect(project).to_not be_done
   end
+
+  it "marks a project done if its tasks are done" do
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
+  end
 end
